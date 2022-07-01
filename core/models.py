06adee45addbedbd42c297b241fa0d0f0d739801
Gabriel da Maia Leandro <gabbmaia2004@gmail.com>
2022-07-01 09:26:19 -0300

    commit classe livro

--- core/models.py
+++ core/models.py
@@ -1,3 +1,4 @@
+from curses.ascii import isblank
 from django.db import models
 
 class Categoria(models.Model):
@@ -12,3 +13,17 @@ class Editora(models.Model):
 
     def __str__(self):
         return self.nome
+
+class Autor(models.Model):
+    nome = models.CharField(max_length=100)
+    email = models.EmailField(max_length=50)
+
+    def __str__(self):
+        return self.nome
+
+class Livro(models.Model):
+    titulo = models.CharField(max_length=255)
+    ISBN = models.CharField(max_length=32)
+    quantidade = models.IntegerField()
+    preco = models.DecimalField(max_digits=7, decimal_places=2)
+    categoria = models.CharField(max_length=255)
