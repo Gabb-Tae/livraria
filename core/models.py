ab22421ed1dba0002e5a4357c62093c4abc0ee76
Gabriel da Maia Leandro <gabbmaia2004@gmail.com>
2022-07-07 15:05:26 -0300

    autor e livros

--- core/models.py
+++ core/models.py
@@ -3,7 +3,6 @@ from django.db import models
 
 class Categoria(models.Model):
     descricao = models.CharField(max_length=100)
-    
     def __str__(self):
         return self.descricao
 
@@ -15,15 +14,26 @@ class Editora(models.Model):
         return self.nome
 
 class Autor(models.Model):
-    nome = models.CharField(max_length=100)
-    email = models.EmailField(max_length=50)
+    nome = models.CharField(max_length=255)
+    email = models.EmailField()
 
     def __str__(self):
         return self.nome
 
+    class Meta:
+        verbose_name_plural = "Autores"
+        
 class Livro(models.Model):
     titulo = models.CharField(max_length=255)
-    ISBN = models.CharField(max_length=32)
+    isbn = models.CharField(max_length=32)
     quantidade = models.IntegerField()
     preco = models.DecimalField(max_digits=7, decimal_places=2)
-    categoria = models.ForeignKey()
+    categoria = models.ForeignKey(
+        Categoria, on_delete=models.PROTECT, related_name="livros"
+    )
+    editora = models.ForeignKey(
+        Editora, on_delete=models.PROTECT, related_name="livros"
+    )
+ 
+    def __str__(self):
+        return f'{self.titulo} ({self.quantidade})'
