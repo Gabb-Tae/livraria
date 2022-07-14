cd4aac56e4f5a6e0c2d8fe3d1ef904ce1a929246
Gabriel da Maia Leandro <gabbmaia2004@gmail.com>
2022-07-14 15:07:10 -0300

    Livros

--- livraria/urls.py
+++ livraria/urls.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from django.urls import include, path
 
 from rest_framework.routers import DefaultRouter
-from core import views
+from core.views import CategoairaViewsSet, EditoraViewSet, LivroViewSet
 
 router = DefaultRouter()
-router.register(r'categorias', views.CategoairaViewsSet)
+router.register(r'categorias', CategoairaViewsSet)
+router.register(r'editoras', EditoraViewSet)
+router.register(r'livros', LivroViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
